12e614d667d49fd54ad28b90342b77a48d239f2f
Kevin Llopis <llopis.kevin@outlook.fr>
2023-03-30 18:24:50 +0200

    feat: Get rental property by id

--- src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
+++ src/main/java/fr/esgi/rent/api/RentalPropertyResource.java
@@ -2,11 +2,14 @@ package fr.esgi.rent.api;
 
 import fr.esgi.rent.beans.RentalProperty;
 import fr.esgi.rent.dto.RentalPropertyDto;
+import fr.esgi.rent.exception.NotFoundRentalPropertyException;
 import fr.esgi.rent.mapper.RentalPropertyDtoMapper;
 import fr.esgi.rent.services.RentalPropertiesFileParser;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.Positive;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 
 import java.util.List;
 
@@ -28,5 +31,16 @@ public class RentalPropertyResource {
         List<RentalProperty> rentalProperties = rentalPropertiesFileParser.parse("rentalProperties.csv");
 
         return rentalPropertyDtoMapper.mapToDtoList(rentalProperties);
+    }
+
+    @GET
+    @Path("/{id}")
+    public RentalPropertyDto getRentalProperty(@PathParam("id") @Positive int id) {
+        return rentalPropertiesFileParser.parse("rentalProperties.csv")
+                .stream()
+                .filter(rentalProperty -> rentalProperty.referenceId() == id)
+                .map(rentalPropertyDtoMapper::mapToDto)
+                .findAny()
+                .orElseThrow(() -> new NotFoundRentalPropertyException("RentalProperty not found with id " + id));
     }
 }
